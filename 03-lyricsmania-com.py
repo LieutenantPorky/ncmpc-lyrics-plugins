7c91bae9a1cfa6b8a0baf2dae26bb3c16cdea2bf
lieutenantporky <jsiniscalco2000@gmail.com>
2022-04-27 20:02:16 +0100

    moved script to python3, and (attempted...) to properly parse the new htmp for lyricsmania

--- 03-lyricsmania-com.py
+++ 03-lyricsmania-com.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 #  Load German lyrics from lyricsmania.com
@@ -35,54 +35,66 @@ parser.add_argument("artist", nargs="?", default="", help="Song artist")
 parser.add_argument("title", nargs="?", default="", help="Song title")
 args = parser.parse_args()
 if args.artist == "":
-    print "Song artist missing"
+    print("Song artist missing")
     sys.exit(1)
 if args.title == "":
-    print "Song title missing"
+    print("Song title missing")
     sys.exit(1)
 
+
+#print("%s/searchnew.php?k=%s+%s" % (base_url, \
+        args.artist.replace(" ","+"), args.title.replace(" ","+")))
+
+
 # load results page
-result_page = download_page("%s/searchnew.php?k=%s+%s" % (base_url, \
+result_page = download_page("%s/search.php?k=%s+%s" % (base_url, \
         args.artist.replace(" ","+"), args.title.replace(" ","+")))
 song_url = ""
 song_id_on_page = ""
 song_part_found = False
-for line in result_page.splitlines():
-    if "Z.html" in line:
+result_page = str(result_page)
+for line in result_page.split(r"\n"):
+    #print(line)
+    if "![]" in line:
         song_part_found = True
         continue
     if song_part_found and "](" in line:
+        #print(line)
         song_url = line.split("](")[1].split(" ")[0]
         break
+#print(song_url)
 if song_url == "":
     sys.exit(2)
 
 # parse songtext website
 result_page = download_page("%s/%s" % (base_url, song_url))
+#print(result_page)
 lyrics = ""
 lyrics_start = False
 empty_line_detected = False
 for line in result_page.splitlines():
-    if line.startswith("### "):
-        if args.artist.lower() != line[4:].lower():
-            sys.exit(3)
-    if "Lyrics to" in line:
+    #print(line)
+    #if line.startswith(b"### "):
+        #if args.artist.lower() != line[4:].lower():
+            #sys.exit(3)
+    if b"rel=\"nofollow\"" in line:
         lyrics_start = True
-        lyrics = "%s\n" % re.sub(r'\*\*.*\*\*(.*)', r'\1', line).strip()
+        lyrics = "%s\n" % re.sub(r'\*\*.*\*\*(.*)', r'\1', str(line)).strip()
         continue
     if lyrics_start and ( \
-            "#####" in line or \
-            line.startswith("[")):
+            b"credits" in line): #or \
+            #line.startswith(b"[")):
         break
+        #print()
     if lyrics_start:
-        line = line.replace("\\", "").strip()
+        line = str(line).replace("\\", "").strip()
         if line == "":
             if empty_line_detected == False:
                 lyrics += "\n"
                 empty_line_detected = True
         else:
-            lyrics += "%s\n" % line
+            lyrics += "%s\n" % str(line)
             empty_line_detected = False
 
-print lyrics[:lyrics.__len__()-1]
+print(lyrics[:lyrics.__len__()-5])
 sys.exit(0)
